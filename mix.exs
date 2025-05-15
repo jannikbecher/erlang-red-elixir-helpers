5ceb1f195e701019a287b53c8368c6bffe08ba00
Gerrit Riessen <gerrit@flowhub.org>
2025-05-15 14:35:10 +0200

    added a rebar.config to try and get exerl to understand the dependencies required here

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ErlangRedHelpers.MixProject do
       app: :erlang_red_helpers,
       version: "0.1.0",
       elixir: "~> 1.18",
-      start_permanent: true,
+      start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
