2c56741a56652670b9dd9efec959948f63446a06
Gerrit Riessen <gerrit@flowhub.org>
2025-05-24 11:12:53 +0200

    too many version numbers and dependencies

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ErlangRedHelpers.MixProject do
   def project do
     [
       app: :erlang_red_helpers,
-      version: "0.1.0",
+      version: "0.1.4",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
